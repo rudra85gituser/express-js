eaee283d2546c876475dddf0abd248371f1e2b81
Rudra Yadav <realrudrayadav@gmail.com>
2024-04-24 23:54:58 +0530

    express progress

---
+++ 1_intro/src/6_serve.js.js
@@ -0,0 +1,32 @@
+
+//we are going to host ,serve ,create website using express js
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+//using builtin middleware we can serve static website
+//builtin middleware - 
+//console.log(__dirname);
+//console.log(path.join(__dirname , '../public'));
+
+
+const staticPath = path.join(__dirname,"../public");
+
+app.use(express.static(staticPath));
+
+//app.get(route,callback)
+app.get("/" , (req,res) => {
+    res.send("hello from the express server");
+});
+
+app.listen(port,()=>{
+     console.log(`listening to the port ${port}`);
+});
+
+
+//in order to do dynamic changes 
+//in order to create dynamic website
+//we can use template engines to get and fetch dynamic data 
+//engines 
